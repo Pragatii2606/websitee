cfe4f396d803168e2c2ede8822cbb4c93a94ecbc
Pragatii2606 <youremail@example.com>
2025-07-25 22:41:55 +0530

    Fix: updated index and vite config for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: "/website/", // 🧠 MUST MATCH your repo name *exactly*
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+  }
+
+  if (command !== 'serve') {
+    config.base = '/websitee/'
+  }
+
+  return config
 })
